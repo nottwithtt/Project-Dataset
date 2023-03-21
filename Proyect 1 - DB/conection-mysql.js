60aebe498fd40dc38b01125524a016e888e8c7e2
JonathanMFV <jonathanporrassandi@estudiantec.cr>
2023-03-20 19:29:17 -0600

    Deleted

--- Proyect 1 - DB/conection-mysql.js
+++
@@ -1,5 +0,0 @@
-const mysql = require('mysql2')
-const connection = mysql.createConnection('mysql://4eme3kqqx4ueltv2mfzj:pscale_pw_L5cO1mxvZ83BMQUwcEYLLMWdEPBPMkrbQE7nB9TEK7t@us-east.connect.psdb.cloud/project?ssl={"rejectUnauthorized":true}'
-)
-console.log('Connected to PlanetScale!')
-connection.end()
