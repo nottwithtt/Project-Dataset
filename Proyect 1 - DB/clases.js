1489be4a87f9c744c5f10ce1e15d78f563a29b08
JonathanMFV <jonathanporrassandi@estudiantec.cr>
2023-03-22 10:53:23 -0600

    Update view

--- Proyect 1 - DB/clases.js
+++ Proyect 1 - DB/clases.js
@@ -5,8 +5,10 @@ function filterChange(filterUser){
 
     if(filterUser == "Users")
         filter.innerHTML = 'Users';
-    if(filterUser == "Datasets")
-        filter.innerHTML = 'Datasets';
+    if(filterUser == "NameDataset")
+        filter.innerHTML = 'Name Dataset';
+    if(filterUser == "DescriptionDataset")
+        filter.innerHTML = 'Description Dataset';
     if(filterUser == "None")
         filter.innerHTML = 'Filter';
 }
@@ -14,8 +16,38 @@ function filterChange(filterUser){
 /* Functions of Profile */
 function editProfile(ActionButton){
     if (ActionButton == "btnEditProfile"){
+        /*Active the button SaveChanges*/
         document.getElementById('btnSaveChanges').style.display='block';
+        /*Desactive the button EditProfile*/
         document.getElementById('btnEditProfile').disabled=true;
+        /*Active the text field Reply Password*/
+        document.getElementById('actionReplyPassword').style.display='block';
+        /*Active fields*/
+        document.getElementById('txtName').disabled=false;
+        document.getElementById('txtLastName').disabled=false;
+        document.getElementById('txtPassword').disabled=false;
+        document.getElementById('txtReplyPassword').disabled=false;
+        document.getElementById('profileBirthdayDate').disabled=false;
     }    
 
 }
+
+
+function saveChanges(ActionButton){
+    if (ActionButton == "btnSaveChanges"){
+        /*Desactive the button SaveChanges*/
+        document.getElementById('btnSaveChanges').style.display='none';
+        /*Active the button EditProfile*/
+        document.getElementById('btnEditProfile').disabled=false;
+        /*Desactive the text field Reply Password*/
+        document.getElementById('actionReplyPassword').style.display='none';
+        /*Active fields*/
+        document.getElementById('txtName').disabled=true;
+        document.getElementById('txtLastName').disabled=true;
+        document.getElementById('txtPassword').disabled=true;
+        document.getElementById('txtReplyPassword').disabled=true;
+        document.getElementById('profileBirthdayDate').disabled=true;
+    }    
+
+}
+
