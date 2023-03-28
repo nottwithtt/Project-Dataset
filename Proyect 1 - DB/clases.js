c60e3a7215d10e79286d82781683b58a46305578
JonathanMFV <jonathanporrassandi@estudiantec.cr>
2023-03-27 18:40:56 -0600

    Update clases.js

--- Proyect 1 - DB/clases.js
+++ Proyect 1 - DB/clases.js
@@ -1,5 +1,31 @@
 // Using onload ="" can call a function when a window is opened
 
+function exampleSession(){
+    updateActiveUser("Jonathan", "Porras", "jonathanps2110","21/10/2003","mkskamska");
+}
+
+function updateActiveUser(name,lastName,username,birthday,photo){
+    sessionStorage.setItem("name", name);
+    sessionStorage.setItem("lastName", lastName);
+    sessionStorage.setItem("username", username);
+    sessionStorage.setItem("birthday", birthday);
+    sessionStorage.setItem("photo", photo);
+}
+
+function updateInactiveUser(name,lastName,username,birthday,photo){
+    activeUser.clear();
+}
+
+function editActiveUser(idUser,name,lastName,password,photo){
+    sessionStorage.clear();
+    sessionStorage.setItem("name", name);
+    sessionStorage.setItem("lastName", lastName);
+    sessionStorage.setItem("username", username);
+    sessionStorage.setItem("birthday", birthday);
+    sessionStorage.setItem("photo", photo);
+}
+
+
 /* Functions of Sign In */
 
 function signIn(){
